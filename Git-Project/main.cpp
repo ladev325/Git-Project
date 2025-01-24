272eb393d8dc5d1aad8b58a22d871cf98871efb6
ladev325 <vlad2008artemov@gmail.com>
2025-01-24 19:11:20 +0200

    Array improvement

--- Git-Project/main.cpp
+++ Git-Project/main.cpp
@@ -3,6 +3,15 @@
 
 
 int main() {
-	PC pc;
-	pc.showInfo();
+	PC* pc = new PC[5]{
+	 {"HP", 2.4, 8, false, 8565},
+	 {"Acer", 2.4, 8, false, 20000},
+	 {"HP2", 2.7, 8, true, 34758},
+	 {"Dell", 2.7, 8, false, 65464},
+	 {"HP", 2.4, 8, true, 78777}
+	};
+	for (int i = 0; i < 5; i++) {
+		pc[i].showInfo();
+		cout << "\n\n\n";
+	}
 }
